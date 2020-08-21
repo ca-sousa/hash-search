0bfe8d337b4831922a05f42061cdd4f8c1312a5f
Carla Sousa <sousa.carla09@gmail.com>
2020-08-21 12:20:39 -0300

    Update the ReadMe

--- README.md
+++ README.md
@@ -4,20 +4,27 @@
 
 <p align="center">Web application for searching hashtags, with Twitter Authentication</p>
 
-Summary
-=================
+# Summary
+
 <!--ts-->
-   * [Instructions](#instructions)
-      * [Back-end](#back)
-      * [Front-end](#front)
-   * [Software](#software)
+
+- [Instructions](#instructions)
+  - [Back-end](#back)
+  - [Front-end](#front)
+- [Software](#software)
 <!--te-->
+
 ### Instructions <a name="instructions"></a>
+
 First make a git clone on this repository, don't forget to install [Node.js](https://nodejs.org/en/), and [Docker](https://docs.docker.com/get-docker/).
+
 ### Back-end <a name="back"></a>
+
 ```bash
 # Go to Server Directory
+# In .env.backup erase .backup and put the authentication keys for Twitter
 $ cd server
+
 
 # Install the dependencies
 $ yarn install
@@ -30,7 +37,9 @@ $ yarn run
 
 # The server will start on the port:3333 - acess <http://localhost:3333>
 ```
+
 ### Front-end <a name="front"></a>
+
 ```bash
 # Go to Web Directory
 $ cd web
@@ -45,11 +54,14 @@ $ yarn run
 ```
 
 ### Software <a name="software"></a>
+
 The following softwares were used in the construction of the project:
+
 - [Node.js](https://nodejs.org/en/)
 - [MongoDB](https://www.mongodb.com/)
 - [Docker](https://www.docker.com/)
 - [React](https://pt-br.reactjs.org/)
 
 ### Status
+
 Building...
--- web/src/pages/Maps/index.jsx
+++ web/src/pages/Maps/index.jsx
@@ -1,40 +1,44 @@
-import * as React from 'react';
-import Header from '../../components/Header';
-import HereMap  from '../../components/HereMap';
+import * as React from "react";
+import Header from "../../components/Header";
+import HereMap from "../../components/HereMap";
 
-import './style.css'
+import "./style.css";
 
 export default class Maps extends React.Component {
-    state = {
-        user: {},
-        error: null,
-        authenticated: false
-    };
+  state = {
+    user: {},
+    error: null,
+    authenticated: false,
+  };
 
-     _handleNotAuthenticated() {
-        this.setState({ authenticated: false });
-    };
-    
-    render () {
-        const { authenticated } = this.state;
-        return (
-            <div id='page-map'>
-                <Header 
-                    authenticated={authenticated}
-                    handleNotAuthenticated={this._handleNotAuthenticated} 
+  _handleNotAuthenticated() {
+    this.setState({ authenticated: false });
+  }
+
+  render() {
+    const { authenticated } = this.state;
+    return (
+      <div id="page-map">
+        <Header
+          authenticated={authenticated}
+          handleNotAuthenticated={this._handleNotAuthenticated}
+        />
+        <main>
+          <div className="input-map">
+            <form>
+              <div className="input">
+                <input
+                  type="text"
+                  id="hashtag"
+                  placeholder="Busque a Hashtag"
                 />
-                <main>
-                    <div className="input-map">
-                        <form>
-                            <div className="input">
-                                <input type='text' id="hashtag" placeholder="Busque a Hashtag"/>
-                                <button type='submit'>Buscar</button>
-                            </div>
-                        </form>
-                    </div>
-                    <HereMap/>
-                </main>
-            </div>
-        );
-    }
+                <button type="submit">Buscar</button>
+              </div>
+            </form>
+          </div>
+          <HereMap />
+        </main>
+      </div>
+    );
+  }
 }
